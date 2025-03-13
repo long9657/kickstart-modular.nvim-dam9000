3c7ff29ab47f47e4e107b918deeee693bee5a091
long9657 <longlehien9657@gmail.com>
2025-03-13 12:18:58 +0700

    new config

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,22 +1,22 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
-  lazy = false,
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  config = function()
-    require('nvim-tree').setup {
-      sync_root_with_cwd = true,
-      -- view = {
-      --   side = 'right',
-      --   signcolumn = 'no',
-      -- },
-      update_focused_file = {
-        enable = true,
-        update_root = true,
-        ignore_list = { 'help' },
-      },
-    }
-  end,
+  -- 'nvim-tree/nvim-tree.lua',
+  -- version = '*',
+  -- lazy = false,
+  -- dependencies = {
+  --   'nvim-tree/nvim-web-devicons',
+  -- },
+  -- config = function()
+  --   require('nvim-tree').setup {
+  --     sync_root_with_cwd = true,
+  --     -- view = {
+  --     --   side = 'right',
+  --     --   signcolumn = 'no',
+  --     -- },
+  --     update_focused_file = {
+  --       enable = true,
+  --       update_root = true,
+  --       ignore_list = { 'help' },
+  --     },
+  --   }
+  -- end,
 }
