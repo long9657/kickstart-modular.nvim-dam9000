9c52d307986fc97af4a7c235e669e3f512221983
long9657 <longlehien9657@gmail.com>
2025-03-15 03:09:10 +0700

    new config

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -17,46 +17,16 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
       require('mini.comment').setup()
-      -- local function current_time()
-      --   return os.date '%H:%M'
-      -- end
-      -- -- Simple and easy statusline.
-      -- --  You could remove this setup call if you don't like it,
-      -- --  and try some other statusline plugin
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
       -- local statusline = require 'mini.statusline'
-      -- -- set use_icons to true if you have a Nerd Font
+      -- set use_icons to true if you have a Nerd Font
       -- statusline.setup { use_icons = vim.g.have_nerd_font }
-      -- statusline.setup {
-      --   content = {
-      --     active = function()
-      --       local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
-      --       local git = MiniStatusline.section_git { trunc_width = 75 }
-      --       local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
-      --       local filename = MiniStatusline.section_filename { trunc_width = 140 }
-      --       local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
-      --       local location = MiniStatusline.section_location { trunc_width = 75 }
-      --       local clock = current_time()
-      --
-      --       return MiniStatusline.combine_groups {
-      --         { hl = mode_hl, strings = { mode } },
-      --         { hl = 'MiniStatuslineClock', strings = { clock } },
-      --         '%<', -- Mark general truncate point
-      --         { hl = 'MiniStatuslineFilename', strings = { filename } },
-      --         '%=', -- End left alignment, start right alignment
-      --         { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-      --         { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
-      --         { hl = 'MiniStatuslineLocation', strings = { location } },
-      --       }
-      --     end,
-      --   },
-      --   set_vim_settings = true,
-      -- }
-      -- vim.cmd [[highlight MiniStatuslineClock guifg=#FF8800]]
-      --
-      -- -- You can configure sections in the statusline by overriding their
-      -- -- default behavior. For example, here we set the section for
-      -- -- cursor location to LINE:COLUMN
-      -- ---@diagnostic disable-next-line: duplicate-set-field
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
       -- statusline.section_location = function()
       --   return '%2l:%-2v'
       -- end
