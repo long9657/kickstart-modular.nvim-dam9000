ac0946596e4a0617db275baa0112ae461981be07
long9657 <longlehien9657@gmail.com>
2025-04-01 02:07:39 +0700

    new config

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,22 +3,15 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      section_separators = '',
-      component_separators = '',
+      theme = 'OceanicNext',
+      -- section_separators = '',
+      -- component_separators = '',
       disabled_filetypes = { 'neo-tree' },
     },
     sections = {
-      lualine_a = {
-        {
-          'mode',
-          color = { fg = '#ffffff' },
-        },
-      },
       lualine_z = {
         {
           'datetime',
-          -- options: default, us, uk, iso, or your own format string ("%H:%M", etc..)
-          color = { fg = '#ffffff' },
           style = 'default',
         },
       },
