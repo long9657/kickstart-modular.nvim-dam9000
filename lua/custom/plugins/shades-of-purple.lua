d7c89133d4f927ee35a8688141d9bd317255d7de
long9657 <longlehien9657@gmail.com>
2025-04-05 17:13:01 +0700

    change that satisfied

--- lua/custom/plugins/shades-of-purple.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'Rigellute/shades-of-purple.vim',
-  config = function()
-    vim.cmd.colorscheme 'shades_of_purple'
-  end,
-}
--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -11,6 +11,7 @@ return {
     -- },
   },
   config = function(_, opts)
+    vim.g.transparent_enabled = not vim.g.neovide
     require('transparent').setup(opts)
     require('transparent').clear_prefix 'neotree'
     require('transparent').clear_prefix 'lualine'
