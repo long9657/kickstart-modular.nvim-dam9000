1da237d2f722ac2ea0795d1db3da67894bd70913
long9657 <longlehien9657@gmail.com>
2025-04-14 20:19:04 +0700

    cmp add border and transparent rewrite

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -3,36 +3,38 @@ return {
   opts = {
     extra_groups = {
       'NormalFloat',
-      'TelescopeBorder',
       'FloatBorder',
       'MsgSeparator',
       'WinSeparator',
+      'TelescopeNormal',
+      'TelescopeBorder',
+      'TelescopePromptTitle',
+      'TelescopePromptBorder',
+      'TelescopePreviewTitle',
+      'TelescopeResultsTitle',
     },
     exclude_groups = {
-      -- 'CursorLine',
-      -- 'CursorLineNr',
-      -- 'NeoTreeCursorLine',
-      'PmenuSel',
+      'CursorLine',
+      'CursorLineNr',
+      'NeoTreeCursorLine',
       'NeoTreeTitleBar',
       'NoiceCusror',
       'NoicePopupmenuSelected',
-      'TelescopePreviewMatch',
-      -- 'NoiceCmdlinePopup',
+      'NoiceCmdlinePopup',
     },
   },
   config = function(_, opts)
-    vim.g.transparent_enabled = true
+    vim.g.transparent_enabled = not vim.g.neovide
     require('transparent').setup(opts)
     require('transparent').clear_prefix 'neotree'
-    require('transparent').clear_prefix 'lualine'
-    require('transparent').clear_prefix 'Pmenu'
-    require('transparent').clear_prefix 'Float'
-    require('transparent').clear_prefix 'Tab'
     require('transparent').clear_prefix 'whichkey'
     require('transparent').clear_prefix 'GitSigns'
     require('transparent').clear_prefix 'Noice'
     require('transparent').clear_prefix 'DiagnosticSign'
     require('transparent').clear_prefix 'Notify'
-    require('transparent').clear_prefix 'Telescope'
+    -- require('transparent').clear_prefix 'lualine'
+    -- require('transparent').clear_prefix 'Pmenu'
+    -- require('transparent').clear_prefix 'Float'
+    -- require('transparent').clear_prefix 'Tab'
   end,
 }
