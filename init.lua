f3d3099577306d0e21095adc7dd81d052b2ea01a
long9657 <longlehien9657@gmail.com>
2025-04-23 03:21:52 +0700

    add padding fix and transparent fix

--- init.lua
+++ init.lua
@@ -63,5 +63,8 @@ require 'options'
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
+-- [[ Padding fix ]]
+-- require 'padding-fix'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -27,19 +27,21 @@ return {
       'NoicePopupmenuSelected',
       'NoiceCmdlinePopup',
     },
+    on_clear = function()
+      require('transparent').clear_prefix 'neotree'
+      require('transparent').clear_prefix 'whichkey'
+      require('transparent').clear_prefix 'GitSigns'
+      require('transparent').clear_prefix 'Noice'
+      require('transparent').clear_prefix 'DiagnosticSign'
+      require('transparent').clear_prefix 'Notify'
+      require('transparent').clear_prefix 'lualine'
+      -- require('transparent').clear_prefix 'Pmenu'
+      -- require('transparent').clear_prefix 'Float'
+      -- require('transparent').clear_prefix 'Tab'
+    end,
   },
   config = function(_, opts)
-    vim.g.transparent_enabled = not vim.g.neovide
     require('transparent').setup(opts)
-    require('transparent').clear_prefix 'neotree'
-    require('transparent').clear_prefix 'whichkey'
-    require('transparent').clear_prefix 'GitSigns'
-    require('transparent').clear_prefix 'Noice'
-    require('transparent').clear_prefix 'DiagnosticSign'
-    require('transparent').clear_prefix 'Notify'
-    require('transparent').clear_prefix 'lualine'
-    -- require('transparent').clear_prefix 'Pmenu'
-    -- require('transparent').clear_prefix 'Float'
-    -- require('transparent').clear_prefix 'Tab'
+    vim.g.transparent_enabled = not vim.g.neovide
   end,
 }
