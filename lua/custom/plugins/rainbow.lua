f867d0573c60843b6d7ef7431d7dfa111ca44f9d
long9657 <longlehien9657@gmail.com>
2025-07-30 11:45:52 +0700

    various updates

--- lua/custom/plugins/rainbow.lua
+++ lua/custom/plugins/rainbow.lua
@@ -1,17 +1,6 @@
 return {
-  -- 'HiPhish/rainbow-delimiters.nvim',
-  'sevenninesixthree/rainbow-delimiters.nvim',
-  branch = 'fix',
-  submodules = false,
+  'HiPhish/rainbow-delimiters.nvim',
   lazy = false,
   main = 'rainbow-delimiters.setup',
-  -- config = function()
-  --   vim.api.nvim_create_autocmd('WinScrolled', {
-  --     callback = function()
-  --       vim.schedule(function()
-  --         require('rainbow-delimiters').enable(0)
-  --       end)
-  --     end,
-  --   })
-  -- end,
+  opts = {},
 }
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -96,7 +96,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
-        'cpptools',
+        'cppdbg',
         'python',
       },
     }
