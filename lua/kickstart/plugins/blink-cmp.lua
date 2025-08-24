553931d51957606b96cae5fcdb14522ed60bed1f
long9657 <longlehien9657@gmail.com>
2025-08-25 02:20:21 +0700

    fix lazydev plugin due to https://github.com/folke/lazydev.nvim/pull/106#issuecomment-3192417100

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -30,7 +30,40 @@ return {
         },
         opts = {},
       },
-      'folke/lazydev.nvim',
+      {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        config = function()
+          -- Monkeypatch in a PR to remove a call to the deprecated `client.notify`
+          -- function.
+          --
+          -- See: https://github.com/folke/lazydev.nvim/pull/106
+          local config = require 'lazydev.config'
+          config.have_0_11 = vim.fn.has 'nvim-0.11' == 1
+
+          local lsp = require 'lazydev.lsp'
+          lsp.update = function(client)
+            lsp.assert(client)
+            if config.have_0_11 then
+              client:notify('workspace/didChangeConfiguration', {
+                settings = { Lua = {} },
+              })
+            else
+              client.notify('workspace/didChangeConfiguration', {
+                settings = { Lua = {} },
+              })
+            end
+          end
+
+          require('lazydev').setup {
+            library = {
+              -- See the configuration section for more details
+              -- Load luvit types when the `vim.uv` word is found
+              { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+            },
+          }
+        end,
+      },
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -77,7 +110,7 @@ return {
             border = 'rounded',
             -- winblend = 0,
           },
-          auto_show = false,
+          auto_show = true,
           auto_show_delay_ms = 500,
         },
         menu = {
