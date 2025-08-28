4738e5bee20418371fad484309c3c1b946f3965d
long9657 <longlehien9657@gmail.com>
2025-08-28 21:12:52 +0700

    change python lsp server

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -229,7 +229,7 @@ return {
         html = {},
         cssls = {},
         -- gopls = {},
-        pyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
